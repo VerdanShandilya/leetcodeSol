fa869922680d4a82dcdf1394a5592d84ee2f65fc
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2023-12-16 17:05:40 +0530

    Time: 5 ms (5.91%) | Memory: 8.7 MB (36.69%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+         int i, key, j;
+    for (i = 1; i < nums.size(); i++) {
+        key = nums[i];
+        j = i - 1;
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            j = j - 1;
+        }
+        nums[j + 1] = key;
+    }
+    return;
+    }
+};
