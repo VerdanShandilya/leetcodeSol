b47e9a194855b12162f4acfc50fefb58c35e3591
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-02-16 11:03:43 +0530

    Time: 917 ms (5.04%) | Memory: 11.3 MB (57.14%) - LeetSync

---
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int,int>> p;
+        for(int i=lo;i<=hi;i++){
+            int count=0;
+            int copy=i;
+            while(copy!=1){
+                if(copy%2==0){
+                    copy=copy/2;
+                    count++;
+                }
+                else{
+                    copy=(copy*3)+1;
+                    count++;
+                }
+            }
+            p.push_back({count,i});
+            sort(p.begin(),p.end());
+        }
+        return p[k-1].second;
+    }
+};
