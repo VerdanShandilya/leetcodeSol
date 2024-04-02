08688e9180a35697c6349107f786b47d604599bb
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-04-02 11:47:21 +0530

    Time: 29 ms (7.05%) | Memory: 9.1 MB (70.43%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        // for(int i=0;i<s.length();i++){
+        //     for(int j=i+1;j<s.length();j++){
+        //         if(s[j]==s[i] && t[j]!=t[i]){
+        //             return false;
+        //         }
+        //         if(s[j]!=s[i] && t[j]==t[i]){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        vector<pair<char,char>> v;
         for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<s.length();j++){
-                if(s[j]==s[i] && t[j]!=t[i]){
-                    return false;
+            auto it =find_if(v.begin(),v.end(), [&](const auto& pair) {
+            return pair.first ==s[i];
+            });
+            auto itt =find_if(v.begin(),v.end(), [&](const auto& pair) {
+            return pair.second ==t[i];
+            });
+            if(it!=v.end()){
+                if(it->second!=t[i]){
+                 return false;
                 }
-                if(s[j]!=s[i] && t[j]==t[i]){
-                    return false;
+            }
+            if(itt!=v.end()){
+                if(itt->first!=s[i]){
+                 return false;
                 }
+            }
+            else{
+                v.push_back(make_pair(s[i], t[i]));
             }
         }
         return true;
