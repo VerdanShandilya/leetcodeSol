522b2224ee70f88ced36aba6571a9456d871e884
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-06-04 15:15:04 +0530

    Time: 7 ms (76.57%) | Memory: 9.7 MB (33.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,32 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         if(s.size()!=t.size()){
+//             return false;
+//         }
+//         for(int i=0;i<min(s.size(),t.size());i++){
+//             if(s[i]!=t[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s.size()!=t.size()){
-            return false;
+        unordered_map<char,int> m;
+        for(auto i : s){
+            m[i]++;
         }
-        for(int i=0;i<min(s.size(),t.size());i++){
-            if(s[i]!=t[i]){
+        for(auto i : t){
+            m[i]--;
+        }
+        for(auto i: m){
+            if(i.second!=0){
                 return false;
             }
         }
