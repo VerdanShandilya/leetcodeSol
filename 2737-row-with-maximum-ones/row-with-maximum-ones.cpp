4b5dbfc5fca3e11fd2a9e81118bd5230936cc581
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-06-06 22:11:42 +0530

    Time: 102 ms (61.98%) | Memory: 64.8 MB (54.74%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,21 +1,49 @@
+// class Solution {
+// public:
+//     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+//         int max=0;
+//         int index=0;
+//         for(int i=0;i<mat.size();i++){
+//             int count=0;
+//             for(int j=0;j<mat[i].size();j++){
+//                 if(mat[i][j]==1){
+//                     count++;
+                    
+//                 }
+//             }
+//             if(count>max){
+//                 max=count;
+//                 index=i;
+//             }
+//         }
+//         return {index,max};
+//     }
+// };
+
+
+
 class Solution {
 public:
+
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int max=0;
-        int index=0;
-        for(int i=0;i<mat.size();i++){
+        int n=mat.size();
+        int m=mat[0].size();
+        int maxcount=-1;
+        int index=-1;
+        vector<int>v;
+        for(int i =0;i<n;i++){
             int count=0;
-            for(int j=0;j<mat[i].size();j++){
-                if(mat[i][j]==1){
-                    count++;
-                    
-                }
+            for(int j=0;j<m;j++){
+                count =count+mat[i][j];
             }
-            if(count>max){
-                max=count;
-                index=i;
+            if(count>maxcount){
+                maxcount=count;
+                index=i;   
             }
         }
-        return {index,max};
+        v.push_back(index);
+        v.push_back(maxcount);
+        return v;
+        
     }
 };
