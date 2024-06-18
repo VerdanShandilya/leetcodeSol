2b2e0327a96e771b06fd7647c1d294c801b5baa0
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-06-18 09:48:18 +0530

    Time: 42 ms (39.18%) | Memory: 17.1 MB (35.13%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,36 +9,36 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // ListNode* temp=headA;
-        // ListNode* temp1=headB;
-        // while(temp!=temp1){
-        //     if(temp==nullptr){
-        //         temp=headB;
-        //     }
-        //     else{
-        //         temp=temp->next;
-        //     }
-        //     if(temp1==nullptr){
-        //         temp1=headA;
-        //     }
-        //     else{
-        //         temp1=temp1->next;
-        //     }
-        // }
-        // return temp1;
-        unordered_map<ListNode*,int> m;
         ListNode* temp=headA;
         ListNode* temp1=headB;
-        while(temp!=nullptr){
-            m[temp]++;
-            temp=temp->next;
-        }
-        while(temp1!=nullptr){
-            if(m[temp1]==1){
-                return temp1;
+        while(temp!=temp1){
+            if(temp==nullptr){
+                temp=headB;
             }
-            temp1=temp1->next;
+            else{
+                temp=temp->next;
+            }
+            if(temp1==nullptr){
+                temp1=headA;
+            }
+            else{
+                temp1=temp1->next;
+            }
         }
-        return nullptr;
+        return temp1;
+        // unordered_map<ListNode*,int> m;
+        // ListNode* temp=headA;
+        // ListNode* temp1=headB;
+        // while(temp!=nullptr){
+        //     m[temp]++;
+        //     temp=temp->next;
+        // }
+        // while(temp1!=nullptr){
+        //     if(m[temp1]==1){
+        //         return temp1;
+        //     }
+        //     temp1=temp1->next;
+        // }
+        // return nullptr;
     }
 };
