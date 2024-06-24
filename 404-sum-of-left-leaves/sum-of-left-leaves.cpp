7b41ec9ea0ef2d5c4314ff678276c8ac6ec3499b
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-06-24 19:58:06 +0530

    Time: 3 ms (55.86%) | Memory: 14.7 MB (81.53%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -1,17 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int ans = 0;
-    int sumOfLeftLeaves(TreeNode* root)
-    {
-        if(root != NULL)
-        {
-            if(root->left != NULL && root->left->left == NULL && root->left->right == NULL)
-            {
-                ans += root->left->val;
-            }
-            sumOfLeftLeaves(root->left);
-            sumOfLeftLeaves(root->right);
+    int ans=0;
+    int helper(TreeNode* root){
+        if(root==nullptr){
+            return 0;
         }
+        if(root->left==root->right){
+            return root->val;
+        }
+        int left=helper(root->left);
+        int right=helper(root->right);
+        ans+=left;
+        return 0;
+
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        helper(root);
         return ans;
     }
 };
