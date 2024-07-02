9cdaaf74157f62ebe35b28f67a6f156f54fc0ded
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-07-03 01:32:54 +0530

    Time: 1114 ms (5.02%) | Memory: 517.9 MB (5.00%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> helper(vector<string> &ans,int o,int c,string a){
+        if(o==0 && c==0){
+            ans.push_back(a);
+            return ans;
+        }
+        if(o==c){
+            helper(ans,o-1,c,a+'(');
+        }
+        else if(o==0){
+           helper(ans,o,c-1,a+')'); 
+        }
+        else if(c==0){
+            helper(ans,o-1,c,a+'(');
+        }
+        else{
+            helper(ans,o-1,c,a+'(');
+            helper(ans,o,c-1,a+')');
+        }
+        return ans;
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        return helper(ans,n,n,"");
+        
+    }
+};
