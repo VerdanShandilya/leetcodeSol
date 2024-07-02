a293ecdc4542fca2a832a68de7b3e6e8a16e9d96
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-07-03 01:37:58 +0530

    Time: 1141 ms (5.02%) | Memory: 517.9 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -11,9 +11,9 @@ public:
         else if(o==0){
            helper(ans,o,c-1,a+')'); 
         }
-        else if(c==0){
-            helper(ans,o-1,c,a+'(');
-        }
+        // else if(c==0){
+        //     helper(ans,o-1,c,a+'(');
+        // }
         else{
             helper(ans,o-1,c,a+'(');
             helper(ans,o,c-1,a+')');
