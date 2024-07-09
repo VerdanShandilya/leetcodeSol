6c5b32734ee25e77760e568f474bc9b7743494d6
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-07-09 09:41:53 +0530

    Time: 14 ms (51.42%) | Memory: 41.6 MB (19.34%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int count=0;
-    void helper(vector<int>& nums, int target,int total,int i){
+    int helper(vector<int>& nums, int target,int total,int i,vector<vector<int>> &dp){
         if(i<0){
             if(target==total){
                 count++;
+                return 1;
             }
-            return;
+            return 0;
         }
-        helper(nums,target,total+nums[i],i-1);
-        helper(nums,target,total-(nums[i]),i-1);
+        if(dp[i][total+1000]!=-1){
+            return dp[i][total+1000];
+        }
+        int a=helper(nums,target,total+nums[i],i-1,dp);
+        int b=helper(nums,target,total-(nums[i]),i-1,dp);
+        return dp[i][total+1000]=a+b;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int total=0;
         for(int i=0;i<nums.size();i++){
             total+=nums[i];
         }
-        helper(nums,target,0,nums.size()-1);
-        return count;
+        vector<vector<int>> dp( nums.size()+1 , vector<int> (2001,-1));  
+        for(int i = 0; i < nums.size()+1; i++) { 
+            for(int j = 0; j <2001; j++) { 
+                dp[i][j] =-1; 
+            } 
+        } 
+        return helper(nums,target,0,nums.size()-1,dp);
     }
 };
