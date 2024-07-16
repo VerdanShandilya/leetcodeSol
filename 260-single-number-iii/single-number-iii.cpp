c9d08e584992d2e24786601b821a6602125fc61b
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-07-16 10:10:04 +0530

    Time: 3 ms (95.98%) | Memory: 12.6 MB (34.01%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int a=0;
+        int num=0;
         for(int i=0;i<nums.size();i++){
-            a=a^nums[i];
+            num=num^nums[i];
         }
-        int mask=1;
-        for(int i=0;i<=31;i++){
-            if((a & mask)==0){
-                mask=mask << 1;
+        int count=0;
+        while(num){
+            if(num & (1<<count)){
+                break;
             }
-            else break;
+            count++;
         }
-        int xor1=0;
-        int xor2=0;
-        for(int i=0;i<nums.size();i++)
-        if(nums[i] & mask){
-            xor1=xor1^nums[i];
+        int bin1=0;
+        int bin2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] & (1<<count)){
+                bin1=bin1^nums[i];
+            }
+            else{
+                bin2=bin2^nums[i];
+            }
         }
-        else{
-            xor2=xor2^nums[i];
-        }
-        return {xor1,xor2};
+        return {bin1,bin2};
     }
 };
