45b0b85870db4ac6d60eb983d3ef665d18129762
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-07-28 03:33:44 +0530

    Time: 23 ms (80.37%) | Memory: 16.1 MB (49.92%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,4 +1,5 @@
 //brute force
+
 // class Solution {
 // public:
 //     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -23,28 +24,73 @@
 //     }
 // };
 
+
 //better
+
+// class Solution {
+// public:
+//     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//         set<vector<int>> s;
+//         for(int i=0;i<nums.size();i++){
+//             for(int j=i+1;j<nums.size();j++){
+//                 set<long> st;
+//                 for(int k=j+1;k<nums.size();k++){
+//                     long sum=nums[i]+nums[j];
+//                     sum+=nums[k];
+//                     long fourth=target-sum;
+//                     if(st.find(fourth)!=st.end()){
+//                         vector<int> temp={nums[i],nums[j],nums[k],(int)fourth};
+//                         sort(temp.begin(),temp.end());
+//                         s.insert(temp);
+//                     }
+//                     st.insert(nums[k]);
+//                 }
+//             }
+//         }
+//         vector<vector<int>> ans(s.begin(),s.end());
+//         return ans;
+//     }
+// };
+
+
+//optimal
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> s;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                set<long> st;
-                for(int k=j+1;k<nums.size();k++){
-                    long long sum=nums[i]+nums[j];
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=nums[i]+nums[j];
                     sum+=nums[k];
-                    long fourth=target-sum;
-                    if(st.find(fourth)!=st.end()){
-                        vector<int> temp={nums[i],nums[j],nums[k],(int)fourth};
-                        sort(temp.begin(),temp.end());
-                        s.insert(temp);
+                    sum+=nums[l];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
-                    st.insert(nums[k]);
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
                 }
             }
         }
-        vector<vector<int>> ans(s.begin(),s.end());
         return ans;
     }
 };
