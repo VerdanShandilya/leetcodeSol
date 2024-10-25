cc164fa86db36fc4856a0a797b3ff92961525f92
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-10-26 00:07:31 +0530

    Time: 294 ms (28.99%) | Memory: 294.7 MB (29.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    int helper(vector<int> &nums,int curr,int prev,vector<vector<int>> &dp){
-        if(curr>=nums.size()){
-            return 0;
+    // int helper(vector<int> &nums,int curr,int prev,vector<vector<int>> &dp){
+    //     if(curr>=nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[curr][prev+1]!=-1){
+    //         return dp[curr][prev+1];
+    //     }
+    //     int pick=0;
+    //     if(prev==-1 || nums[curr]>nums[prev]){
+    //         pick=1+helper(nums,curr+1,curr,dp);
+    //     }
+    //     int nopick=helper(nums,curr+1,prev,dp);
+    //     return dp[curr][prev+1]=max(pick,nopick);
+    // }
+    int helpertab(vector<int> &nums){
+        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,0));
+        for(int curr=nums.size()-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+                int pick=0;
+                if(prev==-1 || nums[curr]>nums[prev]){
+                    pick=1+dp[curr+1][curr+1];
+                }
+                int notpick=dp[curr+1][prev+1];
+                dp[curr][prev+1]=max(pick,notpick);
+            }
         }
-        if(dp[curr][prev+1]!=-1){
-            return dp[curr][prev+1];
-        }
-        int pick=0;
-        if(prev==-1 || nums[curr]>nums[prev]){
-            pick=1+helper(nums,curr+1,curr,dp);
-        }
-        int nopick=helper(nums,curr+1,prev,dp);
-        return dp[curr][prev+1]=max(pick,nopick);
+        return dp[0][0];
     }
     int lengthOfLIS(vector<int>& nums){
-        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size(),-1));
-        return helper(nums,0,-1,dp);
+        // vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size(),-1));
+        return helpertab(nums);
     }
 };
