8dc0d86e91d8444624992e71d96a36b42844ef88
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-10-27 14:29:11 +0530

    Time: 1083 ms (30.00%) | Memory: 465.5 MB (30.00%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
 const int mod=1e9+7;
     int lengthAfterTransformations(string s, int t) {
-        vector<int> v(26,0);
+        vector<long> v(26,0);
         for(int i=0;i<s.size();i++){
             v[s[i]-'a']++;
         }
         for(int i=0;i<t;i++){
-            vector<int> temp(26,0);
+            vector<long> temp(26,0);
             for(int j=0;j<26;j++){
                 if(j==25){
-                    temp[0]=temp[0]+v[25]%mod;
-                    temp[1]=temp[1]+v[25]%mod;
+                    temp[0]=(temp[0]+v[25])%mod;
+                    temp[1]=(temp[1]+v[25])%mod;
                 }
                 else{
-                    temp[j+1]=temp[j+1]+v[j]%mod;
+                    temp[j+1]=(temp[j+1]+v[j])%mod;
                 }
             }
             v=temp;
         }
-        long ans=0;
+        int ans=0;
         for(int i=0;i<v.size();i++){
             ans+=v[i];
             ans%=mod;
