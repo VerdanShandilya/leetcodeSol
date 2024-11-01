f654d8967e5cdf9d40164a3107a7b1c2bb8b5e19
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-11-02 01:55:40 +0530

    Time: 15 ms (36.68%) | Memory: 16.8 MB (41.02%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int helper(int k, vector<int>& prices,int i,int buy,vector<vector<vector<int>>> &dp){
-        if(i==prices.size() || k==0){
+    int helper(vector<int>& prices,int index,int a,int nb,vector<vector<vector<int>>> &dp){
+        if(index>=prices.size() || nb==0){
             return 0;
         }
-        int profit=0;
-        if(dp[i][k][buy]!=-1){
-            return dp[i][k][buy];
+        if(dp[index][a][nb]!=-1){
+            return dp[index][a][nb];
         }
-        if(buy==1){
-            int buy=-prices[i]+helper(k,prices,i+1,0,dp);
-            int notbuy=helper(k,prices,i+1,1,dp);
+        int profit=0;
+        if(a==0){
+            int buy=-prices[index]+helper(prices,index+1,1,nb,dp);
+            int notbuy=helper(prices,index+1,0,nb,dp);
             profit=max(buy,notbuy);
         }
         else{
-            int sell=prices[i]+helper(k-1,prices,i+1,1,dp);
-            int notsell=helper(k,prices,i+1,0,dp);
+            int sell=prices[index]+helper(prices,index+1,0,nb-1,dp);
+            int notsell=helper(prices,index+1,1,nb,dp);
             profit=max(sell,notsell);
         }
-        return dp[i][k][buy]=profit;
+        return dp[index][a][nb]=profit;
     }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(k+1,vector<int> (2,-1)));
-        return helper(k,prices,0,1,dp);
+        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int> (k+1,-1)));
+        return helper(prices,0,0,k,dp);
     }
 };
