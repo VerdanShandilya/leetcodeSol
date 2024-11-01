61f558a3144a3e56811c30aaeab14c5754d29736
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-11-02 01:52:21 +0530

    Time: 438 ms (22.36%) | Memory: 222.5 MB (21.84%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,56 +1,27 @@
-// class Solution {
-// public:
-//     int helper(vector<int>& prices,int index,int a,int nb,vector<vector<vector<int>>> &dp){
-//         if(index>=prices.size() || nb==0){
-//             return 0;
-//         }
-//         if(dp[index][a][nb]!=-1){
-//             return dp[index][a][nb];
-//         }
-//         int buy=0;
-//         int notbuy=0;
-//         int sell=0;
-//         int notsell=0;
-//         if(a==0){
-//             buy=-prices[index]+helper(prices,index+1,1,nb,dp);
-//             notbuy=helper(prices,index+1,0,nb,dp);
-//         }
-//         else{
-//             sell=prices[index]+helper(prices,index+1,0,nb-1,dp);
-//             notsell=helper(prices,index+1,1,nb,dp);
-//         }
-//         return dp[index][a][nb]=max({buy,notbuy,sell,notsell});
-//     }
-//     int maxProfit(vector<int>& prices) {
-//         vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int> (3,-1)));
-//         return helper(prices,0,0,2,dp);
-//     }
-// };
-
 class Solution {
 public:
-    int helper(vector<int>& prices,int i,int k,int buy,vector<vector<vector<int>>> &dp){
-        if(i==prices.size() || k==0){
+    int helper(vector<int>& prices,int index,int a,int nb,vector<vector<vector<int>>> &dp){
+        if(index>=prices.size() || nb==0){
             return 0;
         }
-        int profit=0;
-        if(dp[i][buy][k]!=-1){
-            return dp[i][buy][k];
+        if(dp[index][a][nb]!=-1){
+            return dp[index][a][nb];
         }
-        if(buy==1){
-            int buy=-prices[i]+helper(prices,i+1,k,0,dp);
-            int notbuy=helper(prices,i+1,k,1,dp);
+        int profit=0;
+        if(a==0){
+            int buy=-prices[index]+helper(prices,index+1,1,nb,dp);
+            int notbuy=helper(prices,index+1,0,nb,dp);
             profit=max(buy,notbuy);
         }
         else{
-            int sell=prices[i]+helper(prices,i+1,k-1,1,dp);
-            int notsell=helper(prices,i+1,k,0,dp);
+            int sell=prices[index]+helper(prices,index+1,0,nb-1,dp);
+            int notsell=helper(prices,index+1,1,nb,dp);
             profit=max(sell,notsell);
         }
-        return dp[i][buy][k]=profit;
+        return dp[index][a][nb]=profit;
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
-        return helper(prices,0,2,1,dp);
+        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>> (2,vector<int> (3,-1)));
+        return helper(prices,0,0,2,dp);
     }
 };
