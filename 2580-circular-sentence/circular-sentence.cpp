c00ef7ec534ec0516571123014a0254280b03846
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-11-02 16:16:33 +0530

    Time: 3 ms (10.00%) | Memory: 8.3 MB (31.30%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isCircularSentence(string s) {
-        bool ans=true;
         for(int i=1;i<s.size()-1;i++){
             if(s[i]==' '){
                 if(s[i-1]!=s[i+1]){
-                    ans=false;
-                    return ans;
+                    return false;
                 }
             }
         }
