456d1958c5e642535eb14cdd41fac815951c867b
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-10 17:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (5.79%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int seprator=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]>nums[i+1]){
-                seprator=i;
-                break;
-            }
-        }
-        int i=0;
-        int j=seprator;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(nums[mid]==target){
+    int bs(vector<int>& nums, int k,int l,int r){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==k){
                 return mid;
             }
-            else if(nums[mid]<target){
-                i=mid+1;
+            else if(nums[mid]>k){
+                r=mid-1;
             }
             else{
-                j=mid-1;
-            }
-        }
-        i=seprator+1;
-        j=nums.size()-1;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                i=mid+1;
-            }
-            else{
-                j=mid-1;
+                l=mid+1;
             }
         }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return max(bs(nums,target,0,l),bs(nums,target,l,nums.size()-1));
     }
 };
