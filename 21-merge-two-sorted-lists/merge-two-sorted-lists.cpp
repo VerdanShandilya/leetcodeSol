0ab5340cca72df4349240ea528c447c8e343018f
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-17 16:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (76.92%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -30,15 +30,11 @@ public:
             }
             dummy=dummy->next;
         }
-        while(list1){
+        if(list1){
             dummy->next=list1;
-            list1=list1->next;
-            dummy=dummy->next;
         }
-        while(list2){
+        if(list2){
             dummy->next=list2;
-            list2=list2->next;
-            dummy=dummy->next;
         }
         return ans->next;;
     }
