6d47d0f2e25a7ad891db3e05784ee56e560c0ad4
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-17 20:29:52 +0530

    Time: 38 ms (28.03%) | Memory: 27.4 MB (16.84%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int helper(string& text1, string& text2,int i,int j,vector<vector<int>> &dp){
+    int helper(string &text1, string &text2,int i,int j,vector<vector<int>> &dp){
         if(i<0 || j<0){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int a = 0;
-        if(text1[i] == text2[j]){
-            a = 1+helper(text1,text2,i-1,j-1,dp);
+        int a,b,c;
+        a=b=c=0;
+        if(text1[i]==text2[j]){
+            a=1+helper(text1,text2,i-1,j-1,dp);
         }
         else{
-            a = max(helper(text1,text2,i-1,j,dp),helper(text1,text2,i,j-1,dp));
+            b=helper(text1,text2,i-1,j,dp);
+            c=helper(text1,text2,i,j-1,dp);
         }
-       return dp[i][j] = a;
+        return dp[i][j]=max(a,max(b,c));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp( text1.size() , vector<int> (text2.size()));  
-        for(int i = 0; i < text1.size(); i++) { 
-            for(int j = 0; j < text2.size(); j++) { 
-                dp[i][j] =-1; 
-            } 
-        } 
+        vector<vector<int>> dp(text1.size(),vector<int> (text2.size(),-1));
         return helper(text1,text2,text1.size()-1,text2.size()-1,dp);
     }
 };
