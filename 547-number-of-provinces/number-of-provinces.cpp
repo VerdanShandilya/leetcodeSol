f88af4a182763498b78d096f71ebcbee634222ef
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-23 19:42:44 +0530

    Time: 4 ms (34.45%) | Memory: 20.7 MB (23.22%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>> &adj,vector<bool> &visit,int a){
+    void bfs(vector<vector<int>> &g , vector<bool> &vis ,int node){
+        vis[node]=true;
         queue<int> q;
-        q.push(a);
+        q.push(node);
         while(!q.empty()){
             int temp=q.front();
             q.pop();
-            for(int i=0;i<adj[temp].size();i++){
-                if(visit[adj[temp][i]]!=true){
-                    visit[adj[temp][i]]=true;
-                    q.push(adj[temp][i]);
+            for(int i=0;i<g[temp].size();i++){
+                if(!vis[g[temp][i]]){
+                    vis[g[temp][i]]=true;
+                    q.push(g[temp][i]);
                 }
             }
         }
+        return;
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j] && i!=j){
-                    adj[i].push_back(j);
+        vector<vector<int>> g(isConnected.size());
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected[i].size();j++){
+                int u=isConnected[i][j];
+                if(i!=j && u==1){
+                    g[i].push_back(j);
                 }
             }
         }
-        vector<bool> visit(n,false);
-        int count=0;
-        for(int i=0;i<visit.size();i++){
-            if(visit[i]!=true){
-                count++;
-                bfs(adj,visit,i);
+        vector<bool> vis(isConnected.size(),false);
+        int ans=0;
+        for(int i=0;i<isConnected.size();i++){
+            if(!vis[i]){
+                ans++;
+                bfs(g,vis,i);
             }
         }
-        return count;
+        return ans;
     }
 };
