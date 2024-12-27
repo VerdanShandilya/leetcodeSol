0e6045de1d557e81dbba4415591ac5142c1041cc
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-27 16:34:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (16.63%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int i,int j,int count){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]==0 || (grid[i][j]<count && grid[i][j]>1)){
-            return;
-        }
-        grid[i][j]=count;
-        dfs(grid,i+1,j,count+1);
-        dfs(grid,i-1,j,count+1);
-        dfs(grid,i,j+1,count+1);
-        dfs(grid,i,j-1,count+1);
-        return;
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int x=0;
-        int y=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool> (m,false));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    dfs(grid,i,j,2);
+                    q.push({{i,j},0});
+                    vis[i][j]=true;
                 }
             }
         }
-        int ans=2;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        vector<pair<int, int>> dir = {{-1, 0},{1, 0},{0, -1},{0, 1}};
+        int tm=0;
+        while(!q.empty()){
+            auto temp=q.front();
+            int row=temp.first.first;
+            int col=temp.first.second;
+            int time=temp.second;
+            tm=max(tm,time);
+            q.pop();
+            for(auto temp2 : dir){
+                int nrow=row+temp2.first;
+                int ncol=col+temp2.second;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},time+1});
+                    grid[nrow][ncol]=2;
+                    vis[nrow][ncol]=true;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
                     return -1;
                 }
-                ans=max(ans,grid[i][j]);
             }
         }
-        return ans-2;
+        return tm;
     }
 };
