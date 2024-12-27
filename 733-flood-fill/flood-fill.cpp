af3591f4adafa14724e1b85f889996eabec6f52a
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-27 16:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (9.66%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-   void dfs(vector<vector<int>>& image, int i, int j, int color, int org) {
-    if (i < 0 || j < 0 || i >= image.size() || j >= image[i].size() || image[i][j] != org) {
-        return;
-    }
-    image[i][j] = color;
-    dfs(image, i + 1, j, color, org);
-    dfs(image, i - 1, j, color, org);
-    dfs(image, i, j + 1, color, org);
-    dfs(image, i, j - 1, color, org);
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int org=image[sr][sc];
-        if (org != color) {
-            dfs(image, sr, sc, color, org);
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool> (m,false));
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int scolor=grid[sr][sc];
+        grid[sr][sc]=color;
+        vis[sr][sc]=true;
+        vector<pair<int, int>> dir = {{-1, 0},{1, 0},{0, -1},{0, 1}};
+        while(!q.empty()){
+            auto temp=q.front();
+            int row=temp.first;
+            int col=temp.second;
+            q.pop();
+            for(auto temp2 : dir){
+                int nrow=row+temp2.first;
+                int ncol=col+temp2.second;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==scolor && !vis[nrow][ncol]){
+                    grid[nrow][ncol]=color;
+                    vis[nrow][ncol]=true;
+                    q.push({nrow,ncol});
+                }
+            }
         }
-        return image;
+        return grid;
     }
 };
