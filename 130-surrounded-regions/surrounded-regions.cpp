c8f379d0c38515634982b32bcfa94b419f1cc79d
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-12-29 00:20:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (16.78%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,25 +1,5 @@
 class Solution {
 public:
-    // void bfs(vector<vector<char>>& board,vector<vector<bool>> &vis,int i,int j){
-    //     queue<pair<int,int>> q;
-    //     q.push({i,j});
-    //     vis[i][j]=true;
-    //     vector<pair<int,int>> dir={{-1, 0},{1, 0},{0, -1},{0, 1}};
-    //     while(!q.empty()){
-    //         auto temp=q.front();
-    //         int row=temp.first;
-    //         int col=temp.second;
-    //         q.pop();
-    //         vis[row][col]=true;
-    //         for(auto temp2 : dir){
-    //             int nrow=row+temp2.first;
-    //             int ncol=col+temp2.second;
-    //             if(nrow>=0 && ncol>=0 && nrow<=board.size()-1 && ncol<=board[0].size()-1 && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
-    //                 q.push({nrow,ncol});
-    //             }
-    //         }
-    //     }
-    // }
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
