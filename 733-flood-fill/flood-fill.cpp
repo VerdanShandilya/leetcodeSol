1afdb934a9cc97343a28adcd37d423827e3be0c9
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-25 18:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (5.06%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<bool>> vis(n,vector<bool> (m,false));
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m=image[0].size();
         queue<pair<int,int>> q;
         q.push({sr,sc});
-        int scolor=grid[sr][sc];
-        grid[sr][sc]=color;
+        int co=image[sr][sc];
+        image[sr][sc]=color;
+        vector<vector<bool>> vis(n, vector<bool> (m,false));
+        vector<vector<int>> dir={{-1,0}, {1,0}, {0,-1}, {0,1}};
         vis[sr][sc]=true;
-        vector<pair<int, int>> dir = {{-1, 0},{1, 0},{0, -1},{0, 1}};
         while(!q.empty()){
             auto temp=q.front();
             int row=temp.first;
             int col=temp.second;
             q.pop();
-            for(auto temp2 : dir){
-                int nrow=row+temp2.first;
-                int ncol=col+temp2.second;
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==scolor && !vis[nrow][ncol]){
-                    grid[nrow][ncol]=color;
-                    vis[nrow][ncol]=true;
+            vis[row][col]=true;
+            for(auto j : dir){
+                int nrow=row+j[0];
+                int ncol=col+j[1];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && image[nrow][ncol]==co){
                     q.push({nrow,ncol});
+                    image[nrow][ncol]=color;
                 }
             }
         }
-        return grid;
+        return image;
     }
 };
