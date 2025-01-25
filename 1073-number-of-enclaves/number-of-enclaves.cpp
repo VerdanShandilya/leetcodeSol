b5552fa9f01eb7b1cd6e818861b72b053c7e7828
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-25 22:40:53 +0530

    Time: 12 ms (37.06%) | Memory: 29.4 MB (92.97%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>> &vis){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || vis[i][j] || grid[i][j]==0){
-            return;
-        }
-        if(grid[i][j]==1)
-        vis[i][j]=true;
-        dfs(grid,i+1,j,vis);
-        dfs(grid,i-1,j,vis);
-        dfs(grid,i,j-1,vis);
-        dfs(grid,i,j+1,vis);
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int ans=0;
-        vector<vector<int>> vis(n,vector<int> (m,false));
-        for(int i=0;i<n;i++){
-            if(grid[i][0]==1 && !vis[i][0]){
-                dfs(grid,i,0,vis);
-            }
-            if(grid[i][m-1] && !vis[i][m-1]){
-                dfs(grid,i,m-1,vis);
-            }
-        }
-        for(int j=0;j<m;j++){
-            if(grid[0][j]==1 && !vis[0][j]){
-                dfs(grid,0,j,vis);
-            }
-            if(grid[n-1][j]==1 && !vis[n-1][j]){
-                dfs(grid,n-1,j,vis);
-            }
-        }
+        vector<vector<bool>> vis(n, vector<bool> (m,false));
+        queue<pair<int,int>> q;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]==1)
-                ans++;
+                if((i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1){
+                    q.push({i,j});
+                    vis[i][j]=true;
+                }
+            }
+        }
+
+        vector<pair<int,int>> dir={{-1,0}, {1,0}, {0,1}, {0,-1}};
+
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(auto [x,y] : dir){
+                int nrow=row+x;
+                int ncol=col+y;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=true;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    ans++;
+                }
             }
         }
         return ans;
