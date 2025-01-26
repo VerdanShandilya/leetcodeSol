04c3ed0aa3d15ff328f726ea554dc438e0ef5e7c
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-26 20:53:22 +0530

    Time: 7 ms (37.51%) | Memory: 17.9 MB (83.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& g,vector<bool> &vis,vector<bool> &dfsvis,int node,stack<int> &s){
-        vis[node]=true;
-        dfsvis[node]=true;
-        for(int i=0;i<g[node].size();i++){
-            if(!vis[g[node][i]]){
-                if(dfs(g,vis,dfsvis,g[node][i],s)){
-                    return true;
-                }
-            }
-            else if(dfsvis[g[node][i]]){
-                return true;
-            }
-        }
-        dfsvis[node]=false;
-        s.push(node);
-        return false;
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
         vector<vector<int>> g(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            g[v].push_back(u);
+        for(int i=0;i<pre.size();i++){
+            int u=pre[i][1];
+            int v=pre[i][0];
+            g[u].push_back(v);
         }
-        vector<bool> dfsvis(g.size(),false);
-        vector<bool> vis(g.size(),false);
-        stack<int> s;
+        vector<int> ind(numCourses,0);
         for(int i=0;i<g.size();i++){
-            if(!vis[i]){
-                if(dfs(g,vis,dfsvis,i,s)){
-                    return {};
-                }
+            for(int j=0;j<g[i].size();j++){
+                ind[g[i][j]]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<ind.size();i++){
+            if(!ind[i]){
+                q.push(i);
             }
         }
         vector<int> ans;
-        while(!s.empty()){
-            ans.push_back(s.top());
-            s.pop();
+        while(!q.empty()){
+            auto temp= q.front();
+            q.pop();
+            ans.push_back(temp);
+            for(int i=0;i<g[temp].size();i++){
+                ind[g[temp][i]]--;
+                if(ind[g[temp][i]]==0){
+                    q.push(g[temp][i]);
+                }
+            }
         }
-        return ans;
+        if(ans.size()==numCourses){
+            return ans;
+        }
+        return {};
     }
 };
