49803482335f5f4717bdb5a5c583d39de45b2fe9
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-26 20:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.20%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>> &g,vector<bool> &vis,vector<bool> &visdfs,int node){
+        vis[node]=true;
+        visdfs[node]=true;
+        for(int i=0;i<g[node].size();i++){
+            if(!vis[g[node][i]]){
+                if(dfs(g,vis,visdfs,g[node][i])){
+                    return true;
+                }
+            }
+            else if(visdfs[g[node][i]]){
+                return true;
+            }
+        }
+        visdfs[node]=false;
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
         vector<vector<int>> g(numCourses);
         for(int i=0;i<pre.size();i++){
@@ -7,30 +23,10 @@ public:
             int v=pre[i][0];
             g[u].push_back(v);
         }
-        vector<int> ind(numCourses,0);
-        for(int i=0;i<g.size();i++){
-            for(int j=0;j<g[i].size();j++){
-                ind[g[i][j]]++;
-            }
-        }
-        queue<int> q;
-        for(int i=0;i<ind.size();i++){
-            if(ind[i]==0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            auto temp=q.front();
-            q.pop();
-            for(int i=0;i<g[temp].size();i++){
-                ind[g[temp][i]]--;
-                if(ind[g[temp][i]]==0){
-                    q.push(g[temp][i]);
-                }
-            }
-        }
-        for(int i=0;i<ind.size();i++){
-            if(ind[i]!=0){
+        vector<bool> vis(numCourses,false);
+        vector<bool> visdfs(vis);
+        for(int i=0;i<numCourses;i++){
+            if(dfs(g,vis,visdfs,i)){
                 return false;
             }
         }
