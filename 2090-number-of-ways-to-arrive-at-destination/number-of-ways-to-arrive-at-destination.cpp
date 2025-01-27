970614fc0106a27ca922c5c62ba5add4988b8a6a
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-27 17:08:29 +0530

    Time: 22 ms (11.01%) | Memory: 40.3 MB (19.98%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
+    const int MOD=1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        priority_queue<pair<long long,long long> ,vector<pair<long long,long long>> ,greater<pair<long long,long long>>> pq;
-        vector<vector<pair<int,int>>> g(n);
-        for(int i=0;i<roads.size();i++){
-            int u=roads[i][0];
-            int v=roads[i][1];
-            int wt=roads[i][2];
-            g[u].push_back({v,wt});
-            g[v].push_back({u,wt});
+        vector<long> ans(n,1e12),ways(n,0);
+        vector<vector<pair<long,long>>> g(n);
+        for(auto i : roads){
+            g[i[0]].push_back({i[1],i[2]});
+            g[i[1]].push_back({i[0],i[2]});
         }
-        vector<long long> ans(n,1e12), ways(n,0);
-        pq.push({0,0});
         ans[0]=0;
         ways[0]=1;
-        while(!pq.empty()){
-            auto temp=pq.top();
-            long long wt=temp.first;
-            long long node=temp.second;
-            pq.pop();
-            for(int i=0;i<g[node].size();i++){
-                auto temp2=g[node][i];
-                long long w=temp2.second;
-                long long no=temp2.first;
-                if(ans[no]>w+wt){
-                    ans[no]=w+wt;
-                    pq.push({ans[no],no});
-                    ways[no]=ways[node];
+        priority_queue<pair<long,long>, vector<pair<long,long>>, greater<pair<long,long>>> q;
+        q.push({0,0});
+        while(!q.empty()){
+            long w=q.top().first;
+            long node=q.top().second;
+            q.pop();
+            for(auto i : g[node]){
+                if(w+i.second<ans[i.first]){
+                    ans[i.first]=w+i.second;
+                    q.push({ans[i.first],i.first});
+                    ways[i.first]=ways[node];
                 }
-                else if(ans[no]==w+wt){
-                    ways[no]=(ways[no]+ways[node])%MOD;
+                else if(w+i.second==ans[i.first]){
+                    ways[i.first]=(ways[node]+ways[i.first])%MOD;
                 }
             }
         }
