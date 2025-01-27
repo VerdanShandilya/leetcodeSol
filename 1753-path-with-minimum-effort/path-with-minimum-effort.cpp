9b55dcc60e8737bc3d7ff4e7f381ff3dcc91d57e
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-27 19:58:44 +0530

    Time: 59 ms (47.94%) | Memory: 24.5 MB (53.96%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-        int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        int m =heights.size(), n =heights[0].size();
-        vector<vector<int>> ans(m,vector<int>(n,INT_MAX));
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> q;
+        vector<vector<int>> ans(n, vector<int> (m,INT_MAX));
         ans[0][0]=0;
-        pq.push({0,{0,0}});
-        vector<pair<int,int>> dir={{-1,0},{1,0},{0,-1},{0,1}};
-        while(!pq.empty()){
-            auto temp=pq.top();
-            pq.pop();
-            int weight=temp.first;
-            int row=temp.second.first;
-            int col=temp.second.second;
-            if (row==m-1 && col==n-1) {
-                return weight;
-            }
-            for(auto& temp2 : dir) {
-                int nrow=temp2.first + row;
-                int ncol=temp2.second + col;
-                if (nrow>=0 && ncol>=0 && nrow<m && ncol<n) {
-                    int newEffort = max(weight, abs(heights[row][col] - heights[nrow][ncol]));
-                    if (ans[nrow][ncol]>newEffort) {
-                        ans[nrow][ncol]=newEffort;
-                        pq.push({newEffort,{nrow,ncol}});
+        q.push({0,{0,0}});
+        vector<pair<int,int>> dir={{-1,0}, {1,0}, {0,-1}, {0,1}};
+        while(!q.empty()){
+            int row=q.top().second.first;
+            int col=q.top().second.second;
+            int effort=q.top().first;
+            q.pop();
+
+            for(auto i : dir){
+                int nrow=row+i.first;
+                int ncol=col+i.second;
+                if(nrow>-1 && ncol>-1 && nrow<n && ncol<m){
+                    int w=max(abs(heights[row][col]-heights[nrow][ncol]),effort);
+                    if(ans[nrow][ncol]>w){
+                        ans[nrow][ncol]=w;
+                        q.push({ans[nrow][ncol],{nrow,ncol}});
                     }
                 }
             }
         }
-        return 0;
-    }       
+        return ans[n-1][m-1];
+    }
 };
