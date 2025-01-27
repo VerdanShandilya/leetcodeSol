1dad6e9f46cdf812216ec49384cfdc907ce52601
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-01-28 03:32:08 +0530

    Time: 50 ms (85.37%) | Memory: 76.3 MB (84.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -3,19 +3,35 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int l=0;
         int r=0;
+        if(k==0){
+            return false;
+        }
         unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-            if(m.find(nums[i])!=m.end()){
-                if(abs(m[nums[i]]-i)<=k){
+        while(r<nums.size()){
+            if(r-l<k){
+                if(m.find(nums[r])==m.end()){
+                    m[nums[r]]=r;
+                    r++;
+                }
+                else{
                     return true;
                 }
-                m[nums[i]]=i;
+            }
+            else if(r-l==k){
+                if(m.find(nums[r])==m.end()){
+                    m.erase(nums[l]);
+                    m[nums[r]]=r;
+                    l++;
+                    r++;
+                }
+                else{
+                    return true;
+                }
             }
             else{
-                m[nums[i]]=i;
+                r++;
             }
         }
         return false;
-        
     }
 };
