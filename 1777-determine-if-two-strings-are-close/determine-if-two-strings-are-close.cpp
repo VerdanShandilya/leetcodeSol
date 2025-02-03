56349274c8f30ed7cfaf69b78ecb9cc1ce7ced0f
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-04 02:53:56 +0530

    Time: 16 ms (60.28%) | Memory: 23.5 MB (73.97%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size()!=word2.size()){
-            return false;
-        }
-        unordered_map<char,int> m,m1;
-        vector<pair<int,char>> w1,w2;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
         for(int i=0;i<word1.size();i++){
-            m[word1[i]]++;
-        }
-        for(auto i : m){
-            w1.push_back({i.second,i.first});
+            freq1[word1[i]-'a']++;
         }
         for(int i=0;i<word2.size();i++){
-            m1[word2[i]]++;
-            if(m.find(word2[i])==m.end()){
+            freq2[word2[i]-'a']++;
+        }
+        for(int i=0;i<freq1.size();i++){
+            if((freq1[i]!=0 && freq2[i]==0) || (freq1[i]==0 && freq2[i]!=0)){
                 return false;
             }
         }
-        for(int i=0;i<word2.size();i++){
-            m[word2[i]]--;
-            if(m[word2[i]]==0){
-                m.erase(word2[i]);
-            }
-        }
-        if(m.size()==0){
-            return true;
-        }
-        for(auto i : m1){
-            w2.push_back({i.second,i.first});
-        }
-        if(w1.size()!=w2.size()){
-            return false;
-        }
-        sort(w1.begin(),w1.end());
-        sort(w2.begin(),w2.end());
-        for(int i=0;i<w1.size();i++){
-            auto temp=w1[i];
-            auto temp1=w2[i];
-            if(temp.first!=temp1.first){
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+        for(int i=0;i<freq1.size();i++){
+            if(freq1[i]!=freq2[i]){
                 return false;
             }
         }
