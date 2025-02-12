4a96be856c7fca63b51576d1cba84c7deafb719d
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-12 23:38:50 +0530

    Time: 34 ms (23.68%) | Memory: 24.2 MB (24.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    eraseIslands(grid, i, j);
+    void bfs(vector<vector<char>>& grid,vector<vector<bool>> &vis,int i,int j){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[i][j]=true;
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        vector<pair<int,int>> dir={{-1,0}, {1,0}, {0,-1}, {0,1}};
+        while(!q.empty()){
+            auto temp=q.front();
+            q.pop();
+            int row=temp.first;
+            int col=temp.second;
+            for(auto i : dir){
+                int nrow=row+i.first;
+                int ncol=col+i.second;
+                if(nrow>-1 && ncol>-1 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]=='1'){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=true;
                 }
             }
         }
-        return islands;
     }
-private:
-    void eraseIslands(vector<vector<char>>& grid, int i, int j) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
-            return;
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool> (m,false));
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    ans++;
+                    bfs(grid,vis,i,j);
+                }
+            }
         }
-        grid[i][j] = '0';
-        eraseIslands(grid, i - 1, j);
-        eraseIslands(grid, i + 1, j);
-        eraseIslands(grid, i, j - 1);
-        eraseIslands(grid, i, j + 1);
+        return ans;
     }
 };
