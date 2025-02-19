dfea6c8f8bb63ee41d0e4e3313c382ae57b5cdbd
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-20 00:32:11 +0530

    Time: 55 ms (20.02%) | Memory: 49.7 MB (22.80%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int helper(vector<int>& coins,int index,int amount,vector<vector<int>> &dp){
-        if(index>=coins.size() || amount<0){
+    int helper(vector<int> &coins,int index,int amount,vector<vector<int>> &dp){
+        if(amount==0){
+            return 0;
+        }
+        if(index>=coins.size()){
             return INT_MAX-1;
         }
         if(dp[index][amount]!=-1){
             return dp[index][amount];
         }
-        if(amount==0){
-            return 0;
+
+        int a=INT_MAX;
+        if(amount-coins[index]>=0){
+            a=1+helper(coins,index,amount-coins[index],dp);
         }
-        int pick=1+helper(coins,index,amount-coins[index],dp);
-        int nopick=helper(coins,index+1,amount,dp);
-        return dp[index][amount]=min(pick,nopick);
+        int c=helper(coins,index+1,amount,dp);
+        return dp[index][amount]=min(a,c);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size()+1,vector<int> (amount+1,-1));
+        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
         int ans=helper(coins,0,amount,dp);
         if(ans==INT_MAX-1){
             return -1;
