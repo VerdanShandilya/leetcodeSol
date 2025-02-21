bea39d362481001930bd5a452d0d06d1c9b27f61
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-22 01:33:41 +0530

    Time: 27 ms (27.93%) | Memory: 12.4 MB (17.15%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int helper(string s){
-        unordered_map<char,int> m;
+    int helper(string s,int k){
         int l=0;
         int r=0;
+        int uniq=0;
         int ans=0;
-        int uni=0;
+        unordered_map<char,int> m;
         while(r<s.size()){
             m[s[r]]++;
             if(m[s[r]]==1){
-                uni++;
+                uniq++;
             }
-            if(uni==3){
-                while(uni==3){
-                    ans+=(s.size()-r);
-                    m[s[l]]--;
-                    if(m[s[l]]==0){
-                        uni--;
-                    }
-                    l++;
+            while(uniq>k){
+                m[s[l]]--;
+                if(m[s[l]]==0){
+                    uniq--;
                 }
+                l++;
             }
+            ans+=r-l+1;
             r++;
         }
         return ans;
     }
     int numberOfSubstrings(string s) {
-        return helper(s);
+        return helper(s,3)-helper(s,2);
     }
 };
