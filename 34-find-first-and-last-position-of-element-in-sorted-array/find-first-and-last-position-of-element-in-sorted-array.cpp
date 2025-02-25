6df2ee9165e0852974a00ed8dde9abed76683952
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-25 16:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (86.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,32 +5,32 @@ public:
         int r=nums.size()-1;
         int first=-1;
         while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
-                first=mid;
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target){
+                first = mid;
                 r=mid-1;
             }
-            else if(nums[mid]>target){
-                r=mid-1;
+            else if (nums[mid]<target){
+                l=mid+1;
             }
             else{
-                l=mid+1;
+                r=mid-1;
             }
         }
         int last=-1;
         l=0;
         r=nums.size()-1;
         while(l<=r){
-            int mid=(l+r)/2;;
-            if(nums[mid]==target){
-                last=mid;
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target){
+                last = mid;
                 l=mid+1;
             }
-            else if(nums[mid]>target){
-                r=mid-1;
+            else if (nums[mid]<target){
+                l=mid+1;
             }
             else{
-                l=mid+1;
+                r=mid-1;
             }
         }
         return {first,last};
