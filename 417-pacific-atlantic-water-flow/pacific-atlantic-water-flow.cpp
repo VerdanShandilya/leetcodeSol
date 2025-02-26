e97bb10a35632d106ace5b2fd85348f69baef6cd
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-26 19:14:10 +0530

    Time: 10 ms (48.11%) | Memory: 24.1 MB (42.00%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void helperpacific(vector<vector<int>>& heights,queue<pair<int,int>> &q,vector<vector<bool>> &pacific){
+    void helper(vector<vector<int>>& heights,queue<pair<int,int>> &q,vector<vector<bool>> &pacific){
         int n = heights.size();
         int m = heights[0].size();
         vector<vector<bool>> vis(n, vector<bool> (m,false));
@@ -17,27 +17,6 @@ public:
                 int ncol = col+i.second;
                 if(nrow>-1 && ncol>-1 && nrow<n && ncol<m && !vis[nrow][ncol] && heights[nrow][ncol]>=heights[row][col]){
                     q.push({nrow,ncol});
-                }
-            }
-        }
-    }
-     void helperatlantic(vector<vector<int>>& heights,queue<pair<int,int>> &q1,vector<vector<bool>> &atlantic){
-        int n = heights.size();
-        int m = heights[0].size();
-        vector<vector<bool>> vis(n, vector<bool> (m,false));
-        vector<pair<int,int>> dir= {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        while(!q1.empty()){
-            auto temp = q1.front();
-            int row = temp.first;
-            int col = temp.second;
-            q1.pop();
-            vis[row][col]=true;
-            atlantic[row][col]=true;
-            for(auto i : dir){
-                int nrow = row+i.first;
-                int ncol = col+i.second;
-                if(nrow>-1 && ncol>-1 && nrow<n && ncol<m && !vis[nrow][ncol] && heights[nrow][ncol]>=heights[row][col]){
-                    q1.push({nrow,ncol});
                 }
             }
         }
@@ -61,8 +40,8 @@ public:
                 }
             }
         }
-        helperpacific(heights,q,pacific);
-        helperatlantic(heights,q1,atlantic);
+        helper(heights,q,pacific);
+        helper(heights,q1,atlantic);
         vector<vector<int>> ans;
         for(int i=0;i<heights.size();i++){
             for(int j=0;j<heights[0].size();j++){
