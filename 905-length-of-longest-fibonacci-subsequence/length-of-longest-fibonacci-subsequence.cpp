f6eef9ad08313cd452379aa6ec933cf4ec1f1fa9
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-02-28 01:47:46 +0530

    Time: 400 ms (16.77%) | Memory: 152.4 MB (5.18%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int helper(int i,int j,unordered_map<int,int> &m,vector<int> &arr){
+    int helper(int i,int j,unordered_map<int,int> &m,vector<int> &arr,vector<vector<int>> &dp){
         int count=2;
+        int a = arr[i], b = arr[j];
         while(true){
-            int sum = i+j;
+            int sum = a + b;
             if(m.find(sum)!=m.end()){
-                int temp = i ;
-                i=j;
-                j=sum;
+                a = b;
+                b = sum;
                 count++;
             }
             else{
                 break;
             }
         }
-        return count;
+        return dp[i][j]=count;
     }
     int lenLongestFibSubseq(vector<int>& arr) {
         unordered_map<int,int> m;
+        int maxsize = arr[arr.size()-1] + arr[arr.size()-2];
+        vector<vector<int>> dp(arr.size()+1,vector<int> (arr.size()+1,-1));
         for(int i=0;i<arr.size();i++){
             m[arr[i]]=i;
         }
         int ans=0;
         for(int i=0;i<arr.size();i++){
             for( int j=i+1;j<arr.size();j++){
-                ans=max(ans,helper(arr[i],arr[j],m,arr));
+                ans=max(ans,helper(i,j,m,arr,dp));
             }
         }
         return(ans>=3) ? ans : 0;
