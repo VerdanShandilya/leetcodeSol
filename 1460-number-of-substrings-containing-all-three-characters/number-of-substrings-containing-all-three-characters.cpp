07e24c499856f6a06a10fba3c7423c79dd545321
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-03-12 00:28:15 +0530

    Time: 49 ms (5.72%) | Memory: 17.9 MB (5.52%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int helper(string s,int k){
+    int helper(string s,int size){
         int l=0;
         int r=0;
-        int uniq=0;
+        unordered_map<int,int> m;
         int ans=0;
-        unordered_map<char,int> m;
         while(r<s.size()){
             m[s[r]]++;
-            if(m[s[r]]==1){
-                uniq++;
-            }
-            while(uniq>k){
+            while(m.size()>size){
                 m[s[l]]--;
                 if(m[s[l]]==0){
-                    uniq--;
+                    m.erase(s[l]);
                 }
                 l++;
             }
-            ans+=r-l+1;
+            ans+=(r-l)+1;
             r++;
         }
         return ans;
