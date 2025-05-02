9b6aaca5d74736dbda291ce5a43bfa3e46697afb
Verdan Shandilya <108622840+VerdanShandilya@users.noreply.github.com>
2025-05-03 01:54:00 +0530

    Time: 48 ms (63.90%) | Memory: 29.2 MB (52.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,4 +1,3 @@
-//optimal
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -16,52 +15,21 @@ public:
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
-                    while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;     
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
+                    }
                 }
-                else if(sum<0){
-                    j++;
+                else if(sum>0){
+                    k--;
                 }
                 else{
-                    k--;
+                    j++;
                 }
             }
         }
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-//Recursive Solution
-// class Solution {
-// public:
-//     void dfs(vector<int>& nums, vector<int>&ans,vector<vector<int>>& fans,int i,int sum){
-//         if(sum==0 && ans.size()==3){
-//             fans.push_back(ans);
-//             return;
-//         }
-//         if(i>=nums.size()) return;
-//         ans.push_back(nums[i]);
-//         dfs(nums,ans,fans,i+1,sum+nums[i]);
-//         ans.pop_back();
-//         while(i+1<nums.size() && nums[i]==nums[i+1]){
-//             i=i+1;
-//         }
-//         dfs(nums,ans,fans,i+1,sum);
-        
-//     }
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//         vector<int> ans;
-//         vector<vector<int>> fans;
-//         sort(nums.begin(),nums.end());
-//         dfs(nums,ans,fans,0,0);
-//         return fans;
-//     }
-// };
